b3b62d5562a40cf56079814898db3fa090113803
ben breedlove <bbreedlove@bens-MacBook-Pro.local>
2017-06-05 12:37:37 -0400

    add a preg_replace on image src to point files to wp-content/uploads

--- migrate_database.php
+++ migrate_database.php
@@ -1,8 +1,8 @@
 <?php
 
-#ini_set('memory_limit', '-1');
-#$hostname="127.0.0.1";
+ini_set('memory_limit', '-1');
 $hostname="localhost";
+$hostname="127.0.0.1";
 $username="root";
 $password=$argv[1];
 $d6_db = "mjd6";
@@ -118,6 +118,7 @@ function fix_post_body($html) {
 		$matches = Array();
 
 		$clean = sanititize_file_in_url($src);
+		$clean = preg_replace( '/^\/files\//', '/wp-content/uploads/', $clean );
 		if ($src !== $clean) {
 			$image->setAttribute('src', $clean);
 			print $src . ' is now ' . $clean . "\n\r";
